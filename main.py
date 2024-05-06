38d3dacdc4023c916b01bbc21dcb40692b0edbc1
root <ayberklevendoglu@gmail.com>
2024-05-06 18:07:15 -0400

    finish parse optimize code

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import scapy.all as scapy
+import optparse
 
-arp_request = scapy.ARP(pdst="10.0.2.1/24")
-#scapy.ls(scapy.ARP())
+
+def get_user_input():
+    parse_obj = optparse.OptionParser()
+    parse_obj.add_option("-i", "--ipaddress", dest="ip_address", help="Enter IP address")
+    (user_input, args) = parse_obj.parse_args()
+
+    if not user_input.ip_address:
+        print("Error!Enter IP address")
+
+    return user_input
+
+
+def scan_network(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    combined_packets = broadcast_packet / arp_request
+    (answered, unanswered) = scapy.srp(combined_packets, timeout=1)
+    answered.summary()
+
+
+user_ip_address = get_user_input()
+scan_network(user_ip_address.ip_address)
